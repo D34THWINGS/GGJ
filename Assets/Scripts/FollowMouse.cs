b9e69091d251db16a69c54bc016bf4f9b112fc7b
Benoit DELASSUS <126354@supinfo.com>
2014-01-25 18:44:19 -0500

    cone

---
+++ Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowMouse : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var mouse_pos = Input.mousePosition;
+		mouse_pos.z = 5.23f; //The distance between the camera and object
+		var object_pos = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+		mouse_pos.x = mouse_pos.x - object_pos.x;
+		mouse_pos.y = mouse_pos.y - object_pos.y;
+		float angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler(new Vector3(angle, -90f, 0f));
+	}
+}
