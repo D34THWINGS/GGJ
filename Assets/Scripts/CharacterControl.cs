ffc7232bafc53c413d90d2b23fa4d8bb6f33c316
D34THWINGS <d34things@gmail.com>
2014-01-25 16:16:15 -0500

    Morphing basics

---
+++ Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterControl : MonoBehaviour {
+	public Reshape Reshaper;
+
+	void Start () {
+		Reshaper = GetComponent<Reshape>();
+	}
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.A) && Reshaper.CurrentShape != 1) {
+			Reshaper.PreviousShape = Reshaper.CurrentShape;
+			Reshaper.CurrentShape = 1;
+			print ("To Circle !");
+		}
+		if (Input.GetKeyDown(KeyCode.E) && Reshaper.CurrentShape != 0) {
+			Reshaper.PreviousShape = Reshaper.CurrentShape;
+			Reshaper.CurrentShape = 0;
+			print ("To Square !");
+		}
+	}
+}
---
+++ Assets/Scripts/Reshape.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Reshape : MonoBehaviour {
+	public int CurrentShape {
+		get {
+			return _currentShape;
+		}
+		set {
+			if (value == _currentShape) return;
+
+			if (value == 0) {
+				// Square
+			} else if (value == 1) {
+			// Circle
+			} else {
+				return;
+			}
+
+			_currentShape = value;
+		}
+	}
+	public int PreviousShape {
+		get {
+			return _previousShape;
+		}
+		set {
+			_previousShape = value;
+		}
+	}
+
+	private int _currentShape;
+	private int _previousShape;
+	private Animator animator;
+	
+	void Start () {
+		animator = GetComponent<Animator>();	
+	}
+
+	void Update () {		
+		animator.SetInteger("Shape", CurrentShape);
+		animator.SetInteger("PreviousShape", PreviousShape);	
+	}
+}
