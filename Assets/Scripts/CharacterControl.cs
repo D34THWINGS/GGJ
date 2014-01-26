99b25e0ecffd051dfad1938d520a564723a2360b
D34THWINGS <d34things@gmail.com>
2014-01-26 04:48:23 -0500

    Character Control over 9000 !

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -6,10 +6,12 @@ public class CharacterControl : MonoBehaviour {
 	public float MaxSpeed = 10.0f;
 	public float CameraDistance = 10.0f;
 
+	// Grounding
 	public bool IsGrounded { get; private set; }
-	public bool CollidesLeft { get; private set; }
-	public bool CollidesRight { get; private set; }
-	public GameObject GroundedOn { get; private set; }
+	public Transform GroundTransform;
+	public float GroundRadius = 0.3f;
+	public LayerMask GroundLayers;
+
 	public Reshape Reshaper { get; private set; }
 
 	private bool _doubleJump = true;
@@ -18,11 +20,11 @@ public class CharacterControl : MonoBehaviour {
 	void Start () {
 		Reshaper = GetComponent<Reshape>();
 		IsGrounded = false;
-		CollidesLeft = false;
-		CollidesRight = false;
 	}
 
-	void Update () {
+	void FixedUpdate () {
+		IsGrounded = Physics2D.OverlapCircle(GroundTransform.position, GroundRadius, GroundLayers);
+
 		var move_x = Input.GetAxis("Horizontal");
 		var move = new Vector2(move_x * MaxSpeed, rigidbody2D.velocity.y);
 
@@ -30,9 +32,14 @@ public class CharacterControl : MonoBehaviour {
 			_doubleJump = true;
 		}
 
+		rigidbody2D.velocity = move;
+	}
+
+	void Update () {		
 		if (Input.GetKeyDown(KeyCode.Space)) {
 			if (IsGrounded || (!IsGrounded && _doubleJump)) {
-				move.y = JumpSpeed;
+				rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, JumpSpeed);
+				//rigidbody2D.AddForce(new Vector2(0, 900f));
 
 				if (!IsGrounded) {
 					_doubleJump = false;
@@ -40,41 +47,13 @@ public class CharacterControl : MonoBehaviour {
 			}
 		}
 
-		if ((CollidesLeft && move.x < 0.0f) || (CollidesRight && move.x > 0.0f)) {
-			move.x = 0.0f;
-		}
-
-		rigidbody2D.velocity = move;
-
 		Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, -CameraDistance);
 	}
 
 	void OnCollisionEnter2D (Collision2D theCollision){
-		foreach(ContactPoint2D contact in theCollision.contacts) {
-			if(contact.normal.y >= 1.0f) {
-				IsGrounded = true;
-				GroundedOn = theCollision.gameObject;
-			}
-			if (contact.normal.x <= -1.0f) {
-				CollidesRight = true;
-				_collider = theCollision.gameObject;
-			}
-			if (contact.normal.x >= 1.0f) {
-				CollidesLeft = true;
-				_collider = theCollision.gameObject;
-			}
+		if(theCollision.gameObject.name == "EndDoor"){
+			theCollision.gameObject.GetComponent<ChangeLevel>().ChangeScene();
 		}
-	}
-	
-	void OnCollisionExit2D(Collision2D theCollision){
-		if (theCollision.gameObject == GroundedOn) {
-			GroundedOn = null;
-			IsGrounded = false;
-		}
-		if (theCollision.gameObject == _collider) {
-			CollidesLeft = false;
-			CollidesRight = false;
-			_collider = null;
-		}
+
 	}
 }
