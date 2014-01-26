43d87f218fd024f0d26cc01480d56a5cb1a71a9e
Constant <constant.brunel@supinfo.com>
2014-01-26 04:53:30 -0500

    New Receptacle

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -5,16 +5,19 @@ public class Elevator : MonoBehaviour {
 	public float Speed = 0f;
 	public float LimitDown = -3.5f;
 	public float LimitUp = 10f;
+	public bool Start = false;
 
 	private bool _goesUp = true;
 	
 	// Update is called once per frame
 	void Update () {
-		var y = transform.position.y;
-		if(y > LimitUp || y < LimitDown) {
-			_goesUp = !_goesUp;
+		if(Start) {
+			var y = transform.position.y;
+			if(y > LimitUp || y < LimitDown) {
+				_goesUp = !_goesUp;
+			}
+			var move = new Vector2(0, (_goesUp ? Speed : -Speed));
+			transform.Translate(move * Time.deltaTime);
 		}
-		var move = new Vector2(0, (_goesUp ? Speed : -Speed));
-		transform.Translate(move * Time.deltaTime);
 	}
 }
---
+++ Assets/Scripts/Receptacle/RecElevator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RecElevator : MonoBehaviour {
+	public GameObject Elevator;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter2D(Collision2D theCollision) {
+		print(theCollision.gameObject.name);
+		if(theCollision.gameObject.name == "Support") {
+			Elevator.GetComponent<Elevator>().Start = true;
+		}
+	}
+}
