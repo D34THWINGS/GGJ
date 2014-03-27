f92e6e15108e0fcbfacff551c021bfc3bba83ae1
Benoit DELASSUS <126354@supinfo.com>
2014-03-27 13:53:17 -0400

    level selection ver 2

---
+++ Assets/Scripts/LevelSelection/OnCLickLevel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class OnCLickLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnClick(){
+		Application.LoadLevel(Int32.Parse(gameObject.name.Substring(gameObject.name.Length-1, 1))+1);
+	}
+}
