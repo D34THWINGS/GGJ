da54a7a566448b58f88d01c250a6765dde85e90d
Benoit DELASSUS <126354@supinfo.com>
2014-03-27 00:51:50 -0400

    level selection +NGUI

---
+++ Assets/Scripts/UI/EnableCenterObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableCenterObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.gameObject.GetComponent<UICenterOnChild>().Recenter();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
