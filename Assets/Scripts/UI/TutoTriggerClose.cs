3576a4ff02335998667d422f154d9820c34f4f7e
Benoit DELASSUS <126354@supinfo.com>
2014-04-17 17:41:32 -0400

    Switch panel NGUI tuto

--- Assets/Scripts/UI/TutoTriggerClose.cs
+++ Assets/Scripts/UI/TutoTriggerClose.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class TutoTriggerClose : MonoBehaviour {
 
-	public void OnMouseDown(){
+	public void OnClick(){
 		gameObject.transform.parent.gameObject.SetActive(false);
 		XRay.UI.StaticVariables.isOnTuto = false;
 	}
