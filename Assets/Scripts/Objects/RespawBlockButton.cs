337888b5f6709e0dec0707acff97c3b227a1e7a6
Constant <constant.brunel@supinfo.com>
2014-05-06 16:27:09 -0400

    Respaw System with a button

---
+++ Assets/Scripts/Objects/RespawBlockButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RespawBlockButton : MonoBehaviour {
+
+	public List<GameObject> BlockToRespawn;
+
+	private List<Vector3> OriginPosition;
+	
+	void Start(){
+		OriginPosition = new List<Vector3>();
+		foreach(var block in BlockToRespawn) {
+			OriginPosition.Add(block.transform.position);
+		}
+	}
+	
+	void OnCollisionEnter2D(Collision2D coll){
+		if(coll.gameObject.name == "Player"){
+			var count = 0;
+			foreach(var block in BlockToRespawn) {
+				block.transform.position = OriginPosition[count];
+				print(OriginPosition[count]);
+				count++;
+			}
+		}
+	}
+}
